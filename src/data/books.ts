76f1f39fd03ec79fb4ef281d912b846758fa611f
samshinta <142438884+samshinta@users.noreply.github.com>
2026-03-06 12:12:08 -0300

    Update cover image URL for Elgin AA battery

--- src/data/books.ts
+++ src/data/books.ts
@@ -20,7 +20,7 @@ export const books: Book[] = [
     id: "pilha-elgin-aa-2700mah",
     title: "Pilha Recarregável AA 2700mAh Elgin",
     author: "Elgin",
-    cover: "https://i.imgur.com/XfeljY5.jpg",
+    cover: "https://imgur.com/loMTZju",
     shortDescription: "Pilha recarregável AA 2700mAh Elgin com 4 unidades. Comum.",
     worthIt: `Pilha recarregável AA 2700mAh Elgin com 4 unidades. Comum.`,
     summary: `Pilha recarregável AA 2700mAh Elgin com 4 unidades. Comum.`,
